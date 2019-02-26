889cfa7e993c7f0be4db1d1350dd357b0a97ce3b
timlathy <tim.labushev@gmail.com>
2019-02-26 08:37:15 +0300

    message polling as infinite iteration

--- src/vk.rs
+++ src/vk.rs
@@ -1,5 +1,6 @@
 use serde_derive::Deserialize;
-use serde_json::json;
+
+pub mod long_poll;
 
 pub type VkResult<T> = Result<T, reqwest::Error>;
 
@@ -11,70 +12,44 @@ pub struct VkUser {
     pub id: u64
 }
 
-#[derive(Deserialize, Debug)]
-pub struct VkPollState {
-    server: String,
-    key: String,
-    ts: u64
+#[derive(Debug)]
+pub enum VkMessageOrigin {
+    Chatroom(u64),
+    User(u64)
+}
+
+#[derive(Debug)]
+pub struct VkMessage {
+    origin: VkMessageOrigin,
+    text: String,
+    sender_id: u64
 }
 
 pub struct Vk {
     token: String,
-    chat_id: u64,
     client: reqwest::Client
 }
 
-pub struct VkMessage {
-    text: String,
-    from_id: u64
-}
-
 impl Vk {
-    pub fn new(token: String, chat_id: String) -> Self {
-        Self {
-            token,
-            chat_id: chat_id.parse().unwrap(),
-            client: reqwest::Client::new()
-        }
+    pub fn new(token: String) -> Self {
+        Self { token, client: reqwest::Client::new() }
     }
 
-    pub fn poll_for_messages(&self, poll_state: VkPollState) -> VkResult<(Vec<VkMessage>, VkPollState)> {
-        const MESSAGE_EVENT: u64 = 0;
-
-        let VkPollState { server, key, mut ts } = poll_state;
-
-        let mut resp: serde_json::Value = self.client
-            .get(&format!("https://{}?act=a_check&key={}&ts={}&wait=25&mode=2&version=2", server, key, ts))
-            .send()?
-            .json()?;
-
-        ts = resp["ts"].as_u64().unwrap();
-
-        let messages: Vec<VkMessage> = resp["updates"].as_array().unwrap()
-            .into_iter()
-            .filter_map(|u| u.as_array())
-            .filter(|uf| uf[0] == MESSAGE_EVENT && uf[3] == 2000000000 + self.chat_id)
-            .map(|uf| VkMessage { text: uf[6].as_str().unwrap().to_owned(), from_id: uf[6]["from"].as_u64().unwrap() })
-            .collect();
-
-        Ok((messages, VkPollState { server, key, ts }))
-    }
-
-    pub fn init_long_poll(&self) -> VkResult<VkPollState> {
+    pub fn poll_messages(&self) -> VkResult<long_poll::VkMessagePoller> {
         let mut resp: serde_json::Value = self
             .api_get_method("messages.getLongPollServer", &[("lp_version", "2".to_owned())])
             .send()?
             .json()?;
 
-        Ok(serde_json::from_value(resp["response"].take()).unwrap())
+        Ok(long_poll::VkMessagePoller::new(self, resp["response"].take()))
     }
 
-    pub fn get_chat_members(&self) -> VkResult<Vec<VkUser>> {
+    pub fn get_chat_members(&self, chat_id: u64) -> VkResult<Vec<VkUser>> {
         let mut resp: serde_json::Value = self
             .api_get_method(
                 "messages.getConversationMembers",
                 &[
-                    ("peer_id", format!("{}", 2000000000 + self.chat_id)),
+                    ("peer_id", format!("{}", 2000000000 + chat_id)),
                     ("fields", "screen_name,first_name,last_name".to_owned())
                 ]
             )
