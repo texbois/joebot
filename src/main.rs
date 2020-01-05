0da86ef605b9dc533848a3a4b256101cd449e169
timlathy <tim.labushev@gmail.com>
2020-01-05 07:33:32 +0300

    refactor taki

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 #![feature(slice_patterns)]
 
+mod messages;
+mod storage;
 mod taki;
 mod telegram;
-mod storage;
-
-include!(concat!(env!("OUT_DIR"), "/messages.rs"));
 
 fn main() {
+    let messages = messages::MessageDump::from_file("messages.html");
+
     let bot_token = std::env::var("BOT_TOKEN")
         .expect("Provide a valid bot token via the BOT_TOKEN environment variable");
-    let bot_chat_id: i64 = std::env::var("CHAT_ID").ok().and_then(|id| id.parse().ok())
+    let bot_chat_id: i64 = std::env::var("CHAT_ID")
+        .ok()
+        .and_then(|id| id.parse().ok())
         .expect("Provide the bot's chatroom id via the CHAT_ID environment variable");
 
     let mut redis = storage::Redis::new("redis://127.0.0.1/");
@@ -17,15 +20,22 @@ fn main() {
     let telegram = telegram::Telegram::new(&bot_token);
     let bot_name = telegram.get_bot_username().unwrap();
 
-    println!("@{} is ready. Polling for incoming messages from chat #{}", bot_name, bot_chat_id);
+    println!(
+        "@{} is ready. Polling for incoming messages from chat #{}",
+        bot_name, bot_chat_id
+    );
 
-    let mut game = taki::Taki::new(bot_chat_id, &mut redis);
+    let mut game = taki::Taki::new(&messages, bot_chat_id, &mut redis);
 
     for message in telegram.poll_messages() {
         if message.chat_id != bot_chat_id {
             continue;
         }
-        if let telegram::MessageContents::Command { receiver: Some(ref receiver_name), .. } = message.contents {
+        if let telegram::MessageContents::Command {
+            receiver: Some(ref receiver_name),
+            ..
+        } = message.contents
+        {
             if receiver_name != &bot_name {
                 continue;
             }
--- src/taki.rs
+++ src/taki.rs
@@ -1,125 +1,162 @@
-use crate::{messages, storage, telegram::Message};
-use rand::{SeedableRng, seq::SliceRandom, rngs::SmallRng};
+use crate::{
+    messages::{self, MessageDump},
+    storage, telegram,
+};
+use rand::{rngs::SmallRng, seq::SliceRandom, SeedableRng};
 
 const INIT_SCORE: i32 = 5;
 const MESSAGES_SHOWN: usize = 3;
-const START_MESSAGES: [&'static str; 3] = [
-    "Начнем игру! Вычислите дружка-пирожка по цитаткам:",
-    "Поехали, други! Три цитаты, один чувак из чата - вы знаете, что делать:",
-    "The game must go on! Вычисли приятеля по айпи:"
+const START_MESSAGES: [(&'static str, &'static str); 3] = [
+    ("Один мудрец сказал:", "Кто же это был?"),
+    (
+        "Последний раз подозреваемого видели в местном баре, где он произнес:",
+        "Найдите мне этого пса!",
+    ),
+    ("Дружок-пирожок оставил вам послание:", "Узнали?"),
 ];
 const WIN_MESSAGES: [&'static str; 3] = [
-    "Хорошая работа, приятель.",
-    "Ты справился? Неплохо, дружище.",
-    "Дело сделано, дружочки."
+    "Хорошая работа, дружище.",
+    "А ты неплох, приятель.",
+    "Дело сделано, джентельмены.",
 ];
 const LOSE_MESSAGES: [&'static str; 4] = [
-    "С меня хватит, уроды. В следующий раз удачи!",
-    "Очень жаль, но вы не справились. Я закрываю игру.",
-    "Я вас выслушал, товарищи студенты... А теперь игра окончена.",
-    "Wake up, Neo. You obosralsya. Game over."
+    "Казино не взломано.",
+    "Игра закрыта, неудачники.",
+    "Очень жаль, но вы проиграли.",
+    "Удачи в другой раз, амигос.",
 ];
 
 pub struct Taki<'a> {
-    ongoing: Option<OngoingGame>,
+    messages: &'a MessageDump,
     storage: storage::ChatGameStorage<'a>,
-    rng: SmallRng
+    ongoing: Option<OngoingGame<'a>>,
+    rng: SmallRng,
 }
 
-struct OngoingGame {
-    screen_name: &'static str,
-    full_name: &'static str,
-    full_name_trunc: &'static str,
-    score: i32
+struct OngoingGame<'a> {
+    suspect: &'a messages::Author,
+    score: i32,
 }
 
 impl<'a> Taki<'a> {
-    pub fn new(chat_id: i64, redis: &'a mut storage::Redis) -> Self {
+    pub fn new(messages: &'a MessageDump, chat_id: i64, redis: &'a mut storage::Redis) -> Self {
         Self {
-            ongoing: None,
+            messages,
             storage: redis.get_game_storage("taki", chat_id),
-            rng: SmallRng::from_entropy()
+            ongoing: None,
+            rng: SmallRng::from_entropy(),
         }
     }
 
-    pub fn process_with_reply(&mut self, message: &Message) -> Option<String> {
+    pub fn process_with_reply(&mut self, message: &telegram::Message) -> Option<String> {
         use crate::telegram::MessageContents::*;
 
         match (&message.contents, &mut self.ongoing) {
             (&Command { ref command, .. }, None) if command == "takistart" => {
-                let ((screen_name, full_name, full_name_trunc), messages) = pick_random_target(&mut self.rng);
+                let (suspect, messages) = pick_random_suspect(self.messages, &mut self.rng);
 
                 self.ongoing = Some(OngoingGame {
-                    screen_name,
-                    full_name,
-                    full_name_trunc,
-                    score: INIT_SCORE
+                    suspect,
+                    score: INIT_SCORE,
                 });
+                let (start_prefix, start_suffix) = START_MESSAGES.choose(&mut self.rng).unwrap();
 
-                Some(format!("{}\n\n* {}", START_MESSAGES.choose(&mut self.rng).unwrap(), messages.join("\n* ")))
-            },
+                Some(format!(
+                    "{}\n\n* {}\n\n{}",
+                    start_prefix,
+                    messages.join("\n* "),
+                    start_suffix
+                ))
+            }
             (&Command { ref command, .. }, _) if command == "takistats" => {
-                let stats = self.storage.fetch_sorted_set("scores").unwrap()
-                    .into_iter().enumerate()
+                let stats = self
+                    .storage
+                    .fetch_sorted_set("scores")
+                    .unwrap()
+                    .into_iter()
+                    .enumerate()
                     .map(|(index, (name, score))| format!("{}) {} -- {}", index + 1, name, score))
                     .collect::<Vec<_>>()
                     .join("\n");
 
                 Some(format!("Статы:\n{}", stats))
-            },
+            }
             (&Command { ref command, .. }, _) if command == "takisuspects" => {
-                let suspects = messages::SCREEN_NAMES.iter()
-                    .enumerate()
-                    .zip(messages::FULL_NAMES.iter())
-                    .zip(messages::FULL_NAMES_TRUNC.iter())
-                    .map(|(((idx, screen_name), full_name), full_name_trunc)|
-                         format!("{}) {} под псевдонимами \"{}\", \"{}\"", idx + 1, full_name, screen_name, full_name_trunc)
-                    )
-                    .collect::<Vec<String>>()
-                    .join("\n");
-
+                let suspects = list_suspects(self.messages).join("\n");
                 Some(format!("Подозреваемые:\n{}", suspects))
-            },
+            }
             (&Text(ref text), Some(ref mut game)) => {
                 let text_lower = text.to_lowercase();
 
-                if text_lower.starts_with(game.screen_name) || text_lower.starts_with(game.full_name_trunc) {
-                    let reply = format!("{}\n{} +{}", WIN_MESSAGES.choose(&mut self.rng).unwrap(), message.sender, game.score);
+                if text_lower == game.suspect.short_name || text_lower == game.suspect.full_name {
+                    let reply = format!(
+                        "{}\n{} +{}",
+                        WIN_MESSAGES.choose(&mut self.rng).unwrap(),
+                        message.sender,
+                        game.score
+                    );
 
-                    self.storage.incr_in_set("scores", &message.sender, game.score).unwrap();
+                    self.storage
+                        .incr_in_set("scores", &message.sender, game.score)
+                        .unwrap();
                     self.ongoing = None;
 
                     Some(reply)
-                }
-                else {
+                } else {
                     game.score -= 1;
 
                     if game.score == 0 {
-                        let reply = format!("{}\nЭто был {} ({})", LOSE_MESSAGES.choose(&mut self.rng).unwrap(), game.full_name, game.screen_name);
+                        let reply = format!(
+                            "{}\nЭто был {} ({})",
+                            LOSE_MESSAGES.choose(&mut self.rng).unwrap(),
+                            game.suspect.full_name,
+                            game.suspect.short_name
+                        );
 
                         self.ongoing = None;
 
                         Some(reply)
-                    }
-                    else {
+                    } else {
                         None
                     }
                 }
-            },
-            _ => None
+            }
+            _ => None,
         }
     }
 }
 
-fn pick_random_target(rng: &mut SmallRng) -> ((&'static str, &'static str, &'static str), Vec<&'static str>) {
-    let screen_name = messages::SCREEN_NAMES.choose(rng).unwrap();
+fn list_suspects(messages: &MessageDump) -> Vec<String> {
+    messages
+        .authors
+        .iter()
+        .enumerate()
+        .map(|(idx, author)| {
+            format!(
+                "{}) {} под псевдонимом \"{}\"",
+                idx + 1,
+                author.full_name,
+                author.short_name,
+            )
+        })
+        .collect()
+}
 
-    let (full_name, full_name_trunc, all_messages) =
-        messages::get_full_name_full_name_trunc_messages(screen_name).unwrap();
-
-    let message_sample: Vec<&str> = all_messages
+fn pick_random_suspect<'a>(
+    messages: &'a MessageDump,
+    rng: &mut SmallRng,
+) -> (&'a messages::Author, Vec<&'a str>) {
+    let enum_authors = messages.authors.iter().enumerate().collect::<Vec<_>>();
+    let (author_idx, author) = enum_authors.choose(rng).unwrap();
+    let messages_by_author = messages
+        .texts
+        .iter()
+        .filter(|m| m.author_idx == *author_idx)
+        .collect::<Vec<_>>();
+    let sample_messages = messages_by_author
         .choose_multiple(rng, MESSAGES_SHOWN)
-        .cloned().collect();
+        .map(|m| m.text.as_ref())
+        .collect::<Vec<_>>();
 
-    ((screen_name, full_name, full_name_trunc), message_sample)
+    (author, sample_messages)
 }
